05016c759c25cccf86f8ff3aaafa01cf1b9437be
Dona1228 <takumi.whitestone@icloud.com>
2026-07-30 14:47:38 +0900

    fix bugs in readStat.py

--- readStat.py
+++ readStat.py
@@ -4,21 +4,21 @@ import sys
 
 def readStat(filename):
     stat = {}
-    ratio = []
+    ratio = {}
     maxItem = ['None', 0]
     minItem = ['None', sys.maxsize]
 
     with open(filename, 'r') as f:
         for line in f:
             item = line.strip()
-            stat[item] = stat,get(item, 0) +1
+            stat[item] = stat.get(item, 0) +1
 
     total = sum([stat[k] for k in stat])
     for key in stat:
         ratio[key] = float(stat[key])/float(total)
         bar = '*' * int(stat[key])
         if int(stat[key]) > maxItem[1]:
-        maxItem = [key, stat[key]]
+            maxItem = [key, stat[key]]
         if int(stat[key]) < minItem[1]:
             minItem = [key, stat[key]]
         print (f"{key}:\t{stat[key]} ({ratio[key]*100:.1f}%) {bar}" )
